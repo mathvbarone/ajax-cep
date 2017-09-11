0c93e28d0ca611d11912382713a7a5bd4766c3e9
Matheus Barone <matheusvbarone@gmail.com>
2017-09-11 14:40:15 -0300

    Criando a base lógica do sistema

---
+++ scripts.js
@@ -0,0 +1,40 @@
+(() => {
+
+    const ui =  {
+        inputs: {
+            cep: document.querySelector("#cep"),
+            logradouro: document.querySelector("#logradouro"),
+            bairro: document.querySelector("#bairro"),
+            localidade: document.querySelector("#localidade"),
+            uf: document.querySelector("#uf"),
+        }  
+    }
+
+
+  
+  const validateEntry = function(){
+  }  
+
+  const getAddress = () => {
+
+  }
+
+  const validadeLength = () => {
+
+  }
+
+  const getAddressSuccess = () => {
+
+  }
+
+  const getAddressError = () => {
+    
+  }
+
+  const init = function(){
+      cep.addEventListener("input", validateEntry);
+  }();
+
+
+
+})();
